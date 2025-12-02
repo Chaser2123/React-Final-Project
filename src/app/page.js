ac7a02adb864a4fa75dae2756408225910433e3b
Chaser2123 <chase@LAPTOP-EGEVNV8U.localdomain>
2025-12-02 10:49:57 -0700

    added content

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,8 @@ export default function Home() {
   return (
     <>
       <div
-        className="mx-auto p-4 bg-cover bg-center rounded-lg"
-        style={{
-          backgroundImage:
-            "url('https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?auto=format&fit=crop&w=1200&q=80')",
-        }}
+        className="mx-auto p-4 bg-cover rounded-lg bg-[url('./OIP.jpg')] h-100" 
+        style={{ backgroundPosition: "0px -260px" }}
       >
         <div className="bg-black/40 p-6 rounded">
           <h1 className="text-3xl font-bold mb-4 text-white">Welcome to the Flight Booking App</h1>
---
+++ src/components/AppFooter.jsx
@@ -0,0 +1,9 @@
+export default function AppFooter() {
+  return (
+    <footer className="bg-gray-800 text-white py-4 mt-8">
+      <div className="container mx-auto text-center">
+        <p>&copy; {new Date().getFullYear()} Flight Booking App. All rights reserved. ;););)</p>
+      </div>
+    </footer>
+  );
+}
